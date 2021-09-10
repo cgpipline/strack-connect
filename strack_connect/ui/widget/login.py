cfb6fdbe8076166525e0d59df19ecfd713382834
weiwei163@foxmail.com <ylqchengwei5566~>
2021-09-10 18:51:27 +0800

    add: login widget signal

--- strack_connect/ui/widget/login.py
+++ strack_connect/ui/widget/login.py
@@ -1,17 +1,24 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2021 strack
 
+from dayu_widgets.alert import MAlert
 from dayu_widgets.avatar import MAvatar
 from dayu_widgets.line_edit import MLineEdit
 from dayu_widgets.field_mixin import MFieldMixin
 from dayu_widgets.label import MLabel
 from dayu_widgets.push_button import MPushButton
 from dayu_widgets import dayu_theme
-from dayu_widgets.message import MMessage
-from dayu_widgets.qt import QWidget, QPixmap, QVBoxLayout, MPixmap, QFormLayout, Qt, QHBoxLayout
+from dayu_widgets.qt import QWidget, QPixmap, QVBoxLayout, MPixmap, QFormLayout, Qt, QHBoxLayout, Signal
 
 
 class Login(QWidget, MFieldMixin):
+    """Login widget class."""
+    # Login signal with params  username and password.
+    login = Signal(object, object)
+
+    # # Error signal that can be used to present an error message.
+    loginError = Signal(object)
+
     def __init__(self, parent=None):
         super(Login, self).__init__(parent)
         self.setWindowTitle('strack connect')
@@ -30,8 +37,7 @@ class Login(QWidget, MFieldMixin):
         logon_icon.set_dayu_image(MPixmap(
             './../resource/images/desktop_logo.png'
         ))
-        logon_icon.set_dayu_size(180)
-        logon_icon.setFixedSize(180, 120)
+        logon_icon.set_dayu_size(140)
 
         icon_layout.addWidget(logon_icon)
         main_lay.addLayout(icon_layout)
@@ -44,13 +50,13 @@ class Login(QWidget, MFieldMixin):
         form_lay.setVerticalSpacing(16)
 
         self.login_name_input = MLineEdit()
-        self.login_name_input.setPlaceholderText('请输入用户名')
+        self.login_name_input.setPlaceholderText(u'请输入用户名')
 
         self.password_input = MLineEdit().password()
-        self.password_input.setPlaceholderText('请输入密码')
+        self.password_input.setPlaceholderText(u'请输入密码')
 
-        form_lay.addRow(MLabel('登录名：').strong(), self.login_name_input)
-        form_lay.addRow(MLabel('密码：').strong(), self.password_input)
+        form_lay.addRow(MLabel(u'登录名：').strong(), self.login_name_input)
+        form_lay.addRow(MLabel(u'密码：').strong(), self.password_input)
 
         main_lay.addLayout(form_lay)
 
@@ -59,23 +65,45 @@ class Login(QWidget, MFieldMixin):
         login_button = MPushButton(text='登录').primary()
         login_button.clicked.connect(self.slot_handle_login)
         button_layout.addWidget(login_button)
-        button_layout.setContentsMargins(80, 0, 80, 60)
+        button_layout.setContentsMargins(80, 40, 80, 20)
 
         main_lay.addLayout(button_layout)
+
+        # error message region
+        error_message_layout = QHBoxLayout()
+        self.register_field('msg', '')
+        self.register_field('msg_type', MAlert.InfoType)
+        error_label = MAlert(parent=self)
+        error_label.set_closable(True)
+        self.bind('msg', error_label, 'dayu_text')
+        self.bind('msg_type', error_label, 'dayu_type')
+
+        error_message_layout.addWidget(error_label)
+        error_message_layout.setContentsMargins(20, 0, 20, 0)
+        self.loginError.connect(self.on_set_error)
+
+        main_lay.addLayout(error_message_layout)
+
+        main_lay.addStretch()
+
         self.setLayout(main_lay)
+
+    def on_set_error(self, alert_text, alert_type):
+        """Set the error text and disable the login widget."""
+        self.set_field('msg_type', alert_type)
+        self.set_field('msg', alert_text)
 
     def slot_handle_login(self):
         """Fetch login data from form fields and emit login event."""
         login_name = self.login_name_input.text()
         password = self.password_input.text()
 
-        # 必须输入账户密码
+        # login_name and password require
         if login_name == '' or password == '':
-            MMessage.warning('请输入用户名，密码！', parent=self, closable=True)
+            self.on_set_error(u'请输入用户名，密码！', MAlert.WarningType)
             return
 
-        print("login name {}".format(login_name))
-        print("password {}".format(password))
+        self.login.emit(login_name, password)
 
 
 if __name__ == '__main__':
